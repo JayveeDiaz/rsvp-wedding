66a7ba90a1cafdf9b96ad2f16e5f2e1ef4ca6aee
JayveeDiaz <160087120+JayveeDiaz@users.noreply.github.com>
2025-07-24 01:49:40 +0800

    Test - 1

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enable static export for GitHub Pages
+  basePath: isProd ? "/rsvp-wedding" : "", // Replace with your actual repo name
+  assetPrefix: isProd ? "/rsvp-wedding/" : "", // Same here
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Required for export mode
+  },
 };
 
 export default nextConfig;
