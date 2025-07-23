5cf7f547e8027c269ca577c0f441e63bc601226d
JayveeDiaz <160087120+JayveeDiaz@users.noreply.github.com>
2025-07-24 02:19:07 +0800

    Test-3

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/rsvp-wedding' : '',
-  assetPrefix: isProd ? '/rsvp-wedding/' : '',
-  reactStrictMode: true
+const nextConfig: NextConfig = {
+  output: "export", // Enables static HTML export
+  basePath: isProd ? "/rsvp-wedding" : "",
+  assetPrefix: isProd ? "/rsvp-wedding/" : "",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true // Required for static export to work with Next.js images
+  }
 };
 
 export default nextConfig;
